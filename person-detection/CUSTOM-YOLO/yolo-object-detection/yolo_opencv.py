30dd572f5e63c8e5fb1ce1991dc2e10f1877deb0
aoberai <aoberai05@gmail.com>
2021-02-03 10:31:31 -0800

    did some cleanup of code base and proper person identification working

---
+++ person-detection/CUSTOM-YOLO/yolo-object-detection/yolo_opencv.py
@@ -0,0 +1,105 @@
+#############################################
+# Object detection - YOLO - OpenCV
+# Author : Arun Ponnusamy   (July 16, 2018)
+# Website : http://www.arunponnusamy.com
+############################################
+
+
+import time
+import cv2
+import argparse
+import numpy as np
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-c', '--config', required=True,
+                help='path to yolo config file')
+ap.add_argument('-w', '--weights', required=True,
+                help='path to yolo pre-trained weights')
+ap.add_argument('-cl', '--classes', required=True,
+                help='path to text file containing class names')
+args = ap.parse_args()
+
+
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+
+
+capture = cv2.VideoCapture(0)
+capture.set(3, 640) # resolution of width
+capture.set(4, 480)
+ret, image = capture.read()
+Width = image.shape[1]
+Height = image.shape[0]
+scale = 0.00392
+
+classes = None
+
+with open(args.classes, 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+print(classes)
+color = (255, 255, 255)
+
+while True:
+    initial_time = time.monotonic()
+    ret, image = capture.read()
+    net = cv2.dnn.readNet(args.weights, args.config)
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+
+    outs = net.forward(get_output_layers(net))
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.3:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        cv2.circle(image, (int((x+w)/2), int((y+h)/2)), 5, (150, 150, 150), 10)
+        label = str(classes[class_id])
+        confidence = confidences[i]
+        cv2.putText(image, label + " " + str(round(confidence * 100)) + "%", (int((x+w)/2), int((y+h)/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        cv2.rectangle(image, (int(x), int(y)), (int(x + w), int(y + h)), color, 2)
+    end_time = time.monotonic()
+    fps = 1/(end_time - initial_time)
+    print(fps)
+    cv2.imshow("object detection", image)
+    cv2.waitKey(1)
+
+cv2.imwrite("object-detection.jpg", image)
+cv2.destroyAllWindows()
