9da0af07b56e38edc154010afdd45195a11176bf
luca <luca1230@gmail.com>
2021-02-04 17:10:47 -0800

    removed line detection

--- preprocessing.py
+++ preprocessing.py
@@ -6,22 +6,25 @@ def preprocess(img_location, side):
     # probably put this to something more permanent
     img = cv2.imread(img_location)
     img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    img = cv2.transpose(img)
 
     size_x, size_y, _ = img.shape
     # crop - needs work
     img_crop_size = (480, 480)
     min_resize = max(img_crop_size[0] / size_x, img_crop_size[1] / size_y)
     img = cv2.resize(img, (int(size_x * min_resize), int(size_y * min_resize)))  # keeps the same aspect ratio
-    size_x, size_y = (int(size_x * min_resize), int(size_y * min_resize))
+    print(img.shape)
+    img2 = img
+    size_x, size_y, _ = img.shape
+    print((int(size_x * min_resize), int(size_y * min_resize)))
     if side == 1:
+        # road is on the left so crop it there
         img = img[0:img_crop_size[0], (size_y - img_crop_size[1]):size_y]
     elif side == -1:
-        img = img
+        # road is on the right so crop it there
+        img = img[(size_x - img_crop_size[0]):size_x, (size_y - img_crop_size[1]):size_y]
     else:
-        img = img
-
-    img = cv2.transpose(img)
-    img2 = img
+        img = img[int((size_x - img_crop_size[0]) / 2):int(size_x - (size_x - img_crop_size[0]) / 2), (size_y - img_crop_size[1]):size_y]
 
     edges = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(edges, 200, 300)
@@ -37,11 +40,11 @@ def preprocess(img_location, side):
     b, g, r = cv2.split(img)
     img = cv2.merge((b, g, edges))
 
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            if not -0.3 < (y1 - y2) / (x1 - x2) < 0.3:
-                cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+    # if lines is not None:
+    #     for line in lines:
+    #         x1, y1, x2, y2 = line[0]
+    #         if not -0.3 < (y1 - y2) / (x1 - x2) < 0.3:
+    #             cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
     print(img.shape)
     cv2.imshow('test1', img2)
@@ -50,4 +53,4 @@ def preprocess(img_location, side):
     cv2.destroyAllWindows()
 
 
-preprocess('test2.JPG', 1)
+preprocess('test2.JPG', 0)
