2ec0590aeb7aea8db98ef98342df53124ca7cc78
luca <luca1230@gmail.com>
2021-02-04 17:59:15 -0800

    updated to work with videos

--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,7 @@ import numpy as np
 import cv2
 
 
-def preprocess(img_location, side):
-    # probably put this to something more permanent
-    img = cv2.imread(img_location)
+def preprocess(img, side):
     img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
     img = cv2.transpose(img)
 
@@ -28,8 +26,8 @@ def preprocess(img_location, side):
 
     edges = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(edges, 200, 300)
-    # edges = cv2.GaussianBlur(edges, (3, 3), 0)
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 270, 150, minLineLength=20, maxLineGap=30)
+    edges = cv2.GaussianBlur(edges, (3, 3), 0)
+    # lines = cv2.HoughLinesP(edges, 1, np.pi / 270, 150, minLineLength=20, maxLineGap=30)
 
     img = cv2.medianBlur(img, 5)
 
@@ -39,7 +37,6 @@ def preprocess(img_location, side):
     # remove the blue red layer for smaller image size
     b, g, r = cv2.split(img)
     img = cv2.merge((b, edges, r))
-    cv2.imshow('green', img)
 
     # if lines is not None:
     #     for line in lines:
@@ -49,8 +46,12 @@ def preprocess(img_location, side):
 
     # print(img.shape)
     cv2.imshow('test2', img)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     cv2.destroyAllWindows()
 
 
-preprocess('test2.JPG', 0)
+cap = cv2.VideoCapture('1080p.mov')
+# cap = cv2.VideoCapture(0)
+while True:
+    _, img = cap.read()
+    preprocess(img, 0)
