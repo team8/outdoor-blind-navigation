32de6dd781e9369c1548549477f3baf0216c4ff5
luca <luca1230@gmail.com>
2021-02-04 16:51:00 -0800

    more work trying to make the lines to work

--- preprocessing.py
+++ preprocessing.py
@@ -4,26 +4,31 @@ import cv2
 
 def preprocess(img_location, side):
     # probably put this to something more permanent
-    img = cv2.imread('test.jpg')
+    img = cv2.imread(img_location)
     img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    img_size = (480, 480)
-    # img = cv2.resize(img, img_size)
-    img = cv2.transpose(img)
 
-    print(img.shape)
     size_x, size_y, _ = img.shape
     # crop - needs work
+    img_crop_size = (480, 480)
+    min_resize = max(img_crop_size[0] / size_x, img_crop_size[1] / size_y)
+    img = cv2.resize(img, (int(size_x * min_resize), int(size_y * min_resize)))  # keeps the same aspect ratio
+    size_x, size_y = (int(size_x * min_resize), int(size_y * min_resize))
     if side == 1:
-        img = img[0:img_size[0], (size_y - img_size[1]):size_y]
+        img = img[0:img_crop_size[0], (size_y - img_crop_size[1]):size_y]
     elif side == -1:
         img = img
     else:
         img = img
 
-    edges = cv2.Canny(img, 275, 350)
+    img = cv2.transpose(img)
+    img2 = img
+
+    edges = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(edges, 200, 300)
+    # edges = cv2.GaussianBlur(edges, (3, 3), 0)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 270, 150, minLineLength=20, maxLineGap=30)
 
     img = cv2.medianBlur(img, 5)
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=25, maxLineGap=200)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     edges = cv2.dilate(edges, kernel)
@@ -35,12 +40,14 @@ def preprocess(img_location, side):
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
-            if not -0.2 < (y1 - y2) / (x1 - x2) < 0.2:
+            if not -0.3 < (y1 - y2) / (x1 - x2) < 0.3:
                 cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
+    print(img.shape)
+    cv2.imshow('test1', img2)
     cv2.imshow('test2', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-preprocess('test.jpg', 1)
+preprocess('test2.JPG', 1)
