f14d67d5d55602d02d4696882477d14a9581fce4
Nolan You <nolanyou35@gmail.com>
2021-02-04 23:45:25 -0800

    began to flesh out rough framework. Need to setup test files and actual paths

--- pre-processing/generateData.py
+++ pre-processing/generateData.py
@@ -1,26 +1,34 @@
-import io
 import os
 import random
 
 import cv2
 import numpy as np
 #Need to fill in paths
+import skimage
 import tf as tf
 from tensorflow.python.layers import layers
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from skimage import io
 
-images_path = ""
-augmented_path = ""
+
 data = np.asarray([['left']['middle']['right']])
-def fillDataSet(images_to_generate, path, label):
-    data_augmentation = tf.keras.Sequential([
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(data)
+
+#Generate the one hot encoded path
+data_augmentation = tf.keras.Sequential([
         layers.experimental.preprocessing.RandomRotation(0.2),
         layers.experimental.preprocessing.RandomZoom(0.2, 0.2),
         layers.experimental.preprocessing.RandomContranst(),
         layers.experimental.preprocessing.RanomTranslation()
-    ])
+])
+
+def fillDataSet(images_to_generate, path, label, outpath):
+
     images = []
-    for im in os.listdir(images_path):  # read image from folder, append into images array
-        images.append(os.path.join(images_path, im))
+    for im in os.listdir(path):  # read image from folder, append into images array
+        images.append(os.path.join(path, im))
     i = 1
     # I know that i should start at 0 but it needs to be 1 for clean image titles
     while i <= images_to_generate:
@@ -28,4 +36,8 @@ def fillDataSet(images_to_generate, path, label):
         original_image = io.imread(image)
         transformed_image = random.choice(data_augmentation)(original_image)
 
-
+        new_image_path = "%s/augmented_%s_%s.jpg" %(outpath,label,i)
+        transformed_image = skimage.img_as_ubyte(transformed_image)
+        transformed_image=cv2.cvtColor(transformed_image, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(new_image_path, transformed_image)  # save transformed image to path
+        i += 1
