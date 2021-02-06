884d804b6758e59e3ed6924b4fcf6d0d2e0b6308
aoberai <aoberai05@gmail.com>
2021-02-05 17:22:46 -0800

    works on ubuntu

--- training/jpgFormatter.py
+++ training/jpgFormatter.py
@@ -1,19 +1,21 @@
 # HEIC to JPG image format batch conversion script for Python 3. Tested on Windows 10.
 # You will need to have ImageMagick installed: https://www.imagemagick.org/
+# Copy this script over to the location where the images are stored
+
 
 import os
 import subprocess
 import platform
 
 directory = '.'
-rightDirectory = 'Right'
-leftDirectory = 'Left'
-middleDirectory = 'Middle'
+rightDirectory = 'Right of Sidewalk'
+leftDirectory = 'Left of Sidewalk'
+middleDirectory = 'Middle of Sidewalk'
 unwantedFormats = ('.png', '.heic', '.jpeg')
 platform = platform.system()
 
 for root, dirs, files in os.walk(rightDirectory, topdown=True):
-    for filename in files :
+    for filename in files:
         if filename.lower().endswith(unwantedFormats):
             print('Converting %s...' % os.path.join(root, filename))
             if platform == 'Windows' :
