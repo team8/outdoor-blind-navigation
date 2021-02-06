5cad989ad680dfa992aab8bc888334cc5566295b
Nolan You <nolanyou35@gmail.com>
2021-02-05 16:10:16 -0800

    added in flipping data, have function that fills data set

--- pre-processing/generateData.py
+++ pre-processing/generateData.py
@@ -5,27 +5,45 @@ import cv2
 import numpy as np
 #Need to fill in paths
 import skimage
-import tf as tf
+import tensorflow as tf
 from tensorflow.python.layers import layers
+from tensorflow.keras.layers.experimental import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from skimage import io
+from PIL import Image, ImageOps
 
-
-data = np.asarray([['left']['middle']['right']])
-label_encoder = LabelEncoder()
-integer_encoded = label_encoder.fit_transform(data)
+#
+# data = np.asarray([['left']['middle']['right']])
+# label_encoder = LabelEncoder()
+# integer_encoded = label_encoder.fit_transform(data)
 
 #Generate the one hot encoded path
 data_augmentation = tf.keras.Sequential([
-        layers.experimental.preprocessing.RandomRotation(0.2),
-        layers.experimental.preprocessing.RandomZoom(0.2, 0.2),
-        layers.experimental.preprocessing.RandomContranst(),
-        layers.experimental.preprocessing.RanomTranslation()
+        preprocessing.RandomRotation(0.2),
+        preprocessing.RandomZoom(0.2, 0.2),
+        preprocessing.RandomContrast(0.25),
+        preprocessing.RandomTranslation(0.2,0.2)
 ])
 
-def fillDataSet(images_to_generate, path, label, outpath):
+#This should duplicate everything in a directory and put it into another one
+def flipData(path, label, outpath):
+    images = []
+    for im in os.listdir(path):  # read image from folder, append into images array
+        images.append(os.path.join(path, im))
+    i = 1
+    # I know that i should start at 0 but it needs to be 1 for clean image titles
+    while i <= len(images):
+        image = images[i]
+        original_image = Image.open(image)
+        transformed_image = ImageOps.mirror(original_image)
+        new_image_path = "%s/flipped_%s_%s.jpg" % (outpath, label, i)
+        transformed_image = skimage.img_as_ubyte(transformed_image)
+        transformed_image = cv2.cvtColor(transformed_image, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(new_image_path, transformed_image)  # save transformed image to path
+        i += 1
 
+def fillDataSet(images_to_generate, path, label, outpath):
     images = []
     for im in os.listdir(path):  # read image from folder, append into images array
         images.append(os.path.join(path, im))
@@ -41,3 +59,6 @@ def fillDataSet(images_to_generate, path, label, outpath):
         transformed_image=cv2.cvtColor(transformed_image, cv2.COLOR_BGR2RGB)
         cv2.imwrite(new_image_path, transformed_image)  # save transformed image to path
         i += 1
+
+
+
