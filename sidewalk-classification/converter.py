3129207b589603391108f3b55364e9c0679aee5e
Luca Manolache <luca1230@gmail.com>
2021-02-07 14:07:26 -0800

    fixed changing colors due to BGR vs RGB

--- sidewalk-classification/converter.py
+++ sidewalk-classification/converter.py
@@ -10,12 +10,16 @@ folder = "test"
 def rotate(img_name):
     img = PIL.Image.open(img_name)
 
+    if img is None:
+        return
+
     exif = {
             PIL.ExifTags.TAGS[k]: v
             for k, v in img._getexif().items()
             if k in PIL.ExifTags.TAGS
     }
 
+    img = img.convert('RGB')
     img = np.array(img)
     if exif['Orientation'] == 3:
         img = cv2.rotate(img, cv2.ROTATE_180)
@@ -24,9 +28,10 @@ def rotate(img_name):
     if exif['Orientation'] == 6:
         img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     cv2.imwrite(img_name, img)
-    cv2.waitKey(0)
 
 for file in os.listdir(folder):
     if file.endswith(".JPG") or file.endswith(".jpg"):
+        print(os.path.join(folder, file))
         rotate(os.path.join(folder, file))
