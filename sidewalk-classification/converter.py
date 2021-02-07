7f0c273e5ca4937cd123969cf198a241ca4bcbc6
Luca Manolache <luca1230@gmail.com>
2021-02-07 13:11:32 -0800

    added it to work on a dir

--- sidewalk-classification/converter.py
+++ sidewalk-classification/converter.py
@@ -1,26 +1,32 @@
+import os
 import PIL
 import PIL.Image
 import PIL.ExifTags
 import cv2
 import numpy as np
 
-folder = "image.jpg"
+folder = "test"
 
-img = PIL.Image.open(folder)
+def rotate(img_name):
+    img = PIL.Image.open(img_name)
 
-exif = {
-        PIL.ExifTags.TAGS[k]: v
-        for k, v in img._getexif().items()
-        if k in PIL.ExifTags.TAGS
-}
+    exif = {
+            PIL.ExifTags.TAGS[k]: v
+            for k, v in img._getexif().items()
+            if k in PIL.ExifTags.TAGS
+    }
 
-img = np.array(img)
-if exif['Orientation'] == 3:
-    img = cv2.rotate(img, cv2.ROTATE_180)
-if exif['Orientation'] == 8:
-    img = cv2.rotate(img, cv2.ROTATE_90)
-if exif['Orientation'] == 6:
-    img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    img = np.array(img)
+    if exif['Orientation'] == 3:
+        img = cv2.rotate(img, cv2.ROTATE_180)
+    if exif['Orientation'] == 8:
+        img = cv2.rotate(img, cv2.ROTATE_90)
+    if exif['Orientation'] == 6:
+        img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-cv2.imshow("img", img)
-cv2.waitKey(0)
+    cv2.imwrite(img_name, img)
+    cv2.waitKey(0)
+
+for file in os.listdir(folder):
+    if file.endswith(".JPG") or file.endswith(".jpg"):
+        rotate(os.path.join(folder, file))
