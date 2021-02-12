4e47c1b4e261d40b15961279a213c96f9dd1ce8e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-11 17:25:11 -0800

    deals with None values correctly

--- training/circularBuffer.py
+++ training/circularBuffer.py
@@ -2,6 +2,35 @@ import statistics
 import numpy as np
 
 
+def median(list):
+    sortedlist = sorted(list, key=lambda x: (x is None, x))
+    n = len(sortedlist)
+    if n == 0:
+        raise Exception("no median for empty data")
+    if n % 2 == 1:
+        return sortedlist[n // 2]
+    else:
+        i = n // 2
+        if sortedlist[i] and sortedlist[i - 1]:
+            return (sortedlist[i - 1] + sortedlist[i]) / 2
+        else:
+            return None
+
+
+def mean(list):
+    newlist = []
+    for x in list:
+        if x is not None:
+            newlist.append(x)
+    if iter(newlist) is list:
+        newlist = list(newlist)
+    n = len(newlist)
+    if n < 1:
+        return None
+    total = sum(newlist)
+    return total / n
+
+
 class CircularBuffer:
     def __init__(self, capacity, typeex=0):
         self.capacity = capacity
@@ -10,6 +39,7 @@ class CircularBuffer:
         self.head = 0
         self.type = typeex
         self.size = 0
+        self.none_threshold = 5
 
     def __full(self):
         return self.size == self.capacity
@@ -44,49 +74,74 @@ class CircularBuffer:
         else:
             return len(self.queue[0])
 
-    def __second_dimension_list(self):
+    def __nonethreshold(self, inputlist):
         tmplist = []
+        if isinstance(inputlist, np.ndarray):
+            inputlist = list(inputlist)
         for x in range(self.__second_dimension_capacity()):
             if x is not None:
                 tmplist.append([])
         if isinstance(self.type, np.ndarray):
-            for x in self.queue:
+            for x in (inputlist or []):
                 if x is not None:
+                    tmplist.append(x)
+        else:
+            for x in (inputlist or []):
+                if x is not None:
+                    tmplist.append(x)
+        if tmplist and inputlist:
+            noneamount = len(inputlist) - len(tmplist)
+        elif inputlist:
+            noneamount = len(inputlist)
+        else:
+            noneamount = 0
+        if noneamount >= self.none_threshold:
+            return False
+        else:
+            return True
+
+    def __second_dimension_list(self):
+        tmplist = []
+        for x in range(self.__second_dimension_capacity()):
+            tmplist.append([])
+        if isinstance(self.type, np.ndarray):
+            for x in self.queue:
+                if self.__nonethreshold(x):
                     for y in x:
                         element = list(x).index(y)
                         tmplist[element].extend([y])
         else:
             for x in self.queue:
-                if x is not None:
-                    for y in x:
+                if self.__nonethreshold(x):
+                    for y in (x or []):
                         element = x.index(y)
                         tmplist[element].extend([y])
         return tmplist
 
     def average(self):
         if isinstance(self.type, (float, int)):
-            return statistics.mean(self.queue)
+            return mean(self.queue)
         elif isinstance(self.type, (list, np.ndarray)):
             tmp = self.__second_dimension_list()
             one_d_list = []
             for x in range(len(tmp)):
                 innertmp = tmp[x]
-                mean = statistics.mean(innertmp)
-                one_d_list.append(mean)
+                newmean = mean(innertmp)
+                one_d_list.append(newmean)
             return one_d_list
         else:
             print("Incompatible data type")
 
     def median(self):
         if isinstance(self.type, (float, int)):
-            return statistics.median(self.queue)
+            return median(self.queue)
         elif isinstance(self.type, (list, np.ndarray)):
             tmp = self.__second_dimension_list()
             one_d_list = []
             for x in range(len(tmp)):
                 innertmp = tmp[x]
-                median = statistics.median(innertmp)
-                one_d_list.append(median)
+                innermedian = median(innertmp)
+                one_d_list.append(innermedian)
             return one_d_list
         else:
             print("Incompatible data type")
