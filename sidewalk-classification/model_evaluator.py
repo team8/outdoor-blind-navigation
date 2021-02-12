8c4e423a31ad8bc74f7f3f5f105bca08f166378f
aoberai <aoberai05@gmail.com>
2021-02-12 11:03:20 -0800

    added circular buffer, crcular buffer still broken

--- sidewalk-classification/model_evaluator.py
+++ sidewalk-classification/model_evaluator.py
@@ -5,14 +5,19 @@ from utils.circularBuffer import CircularBuffer
 
 classes = ['Left of Sidewalk', 'Middle of Sidewalk', 'Right of Sidewalk']
 model = tf.keras.models.load_model("sidewalk_classification_model.h5")
-vid = cv2.VideoCapture("/home/aoberai/Downloads/1080p.mov")
+vid = cv2.VideoCapture("/home/aoberai/Downloads/Long_Sidewalk.mp4")
 predictions = CircularBuffer(10, [])
+state = ""
 while True:
-    orig_cap = cv2.flip(vid.read()[1], 1)
+    orig_cap = cv2.resize(cv2.rotate(vid.read()[1], cv2.ROTATE_90_COUNTERCLOCKWISE), (480, 360))
     cap = cv2.resize(orig_cap, (100, 100))
     raw_prediction = (model.predict(np.expand_dims(cap, 0))).tolist()[0]
-    print(raw_prediction)
-    predictions.add(raw_prediction)
-    print(classes[np.argmax(predictions.average())])
-    cv2.imshow("Test", orig_cap)
+    predictions.add([None, None, None] if max(raw_prediction) < 0.9 else raw_prediction)
+    averaged_prediction = np.argmax(predictions.average())
+    if averaged_prediction is None and None not in averaged_prediction:
+        state = "Nothing detected"
+    else:
+        state = classes[np.argmax(predictions.average())]
+    cv2.imshow(state, orig_cap)
     cv2.waitKey(1)
+
--- utils/circularBuffer.py
+++ utils/circularBuffer.py
@@ -1,4 +1,34 @@
 import statistics
+import numpy as np
+
+
+def median(list):
+    sortedlist = sorted(list, key=lambda x: (x is None, x))
+    n = len(sortedlist)
+    if n == 0:
+        raise Exception("no median for empty data")
+    if n % 2 == 1:
+        return sortedlist[n // 2]
+    else:
+        i = n // 2
+        if sortedlist[i] and sortedlist[i - 1]:
+            return (sortedlist[i - 1] + sortedlist[i]) / 2
+        else:
+            return None
+
+
+def mean(list):
+    newlist = []
+    for x in list:
+        if x is not None:
+            newlist.append(x)
+    if iter(newlist) is list:
+        newlist = list(newlist)
+    n = len(newlist)
+    if n < 1:
+        return None
+    total = sum(newlist)
+    return total / n
 
 
 class CircularBuffer:
@@ -9,15 +39,16 @@ class CircularBuffer:
         self.head = 0
         self.type = typeex
         self.size = 0
+        self.none_threshold = 5
 
-    def full(self):
+    def __full(self):
         return self.size == self.capacity
 
-    def empty(self):
+    def __empty(self):
         return self.size == 0
 
     def add(self, item):
-        if self.full():
+        if self.__full():
             removed = self.remove()
             self.tail = (self.tail + 1) % self.capacity
             self.queue[self.tail] = item
@@ -29,7 +60,7 @@ class CircularBuffer:
             self.size = self.size + 1
 
     def remove(self):
-        if self.empty():
+        if self.__empty():
             print("Queue Empty")
         else:
             item = self.queue[self.head]
@@ -37,43 +68,94 @@ class CircularBuffer:
             self.size = self.size - 1
             return item
 
-    def second_dimension_capacity(self):
-        return len(self.queue[0])
+    def __second_dimension_capacity(self):
+        if isinstance(self.type, np.ndarray):
+            return len(self.queue)
+        else:
+            return len(self.queue[0])
 
-    def second_dimension_list(self):
+    def __nonethreshold(self, inputlist):
         tmplist = []
-        for x in range(self.second_dimension_capacity()):
+        if isinstance(inputlist, np.ndarray):
+            inputlist = list(inputlist)
+        for x in range(self.__second_dimension_capacity()):
+            if x is not None:
+                tmplist.append([])
+        if isinstance(self.type, np.ndarray):
+            for x in (inputlist or []):
+                if x is not None:
+                    tmplist.append(x)
+        else:
+            for x in (inputlist or []):
+                if x is not None:
+                    tmplist.append(x)
+        if tmplist and inputlist:
+            noneamount = len(inputlist) - len(tmplist)
+        elif inputlist:
+            noneamount = len(inputlist)
+        else:
+            noneamount = 0
+        if noneamount >= self.none_threshold:
+            return False
+        else:
+            return True
+
+    def __second_dimension_list(self):
+        tmplist = []
+        for x in range(self.__second_dimension_capacity()):
             tmplist.append([])
-        for x in self.queue:
-            for y in x:
-                element = x.index(y)
-                tmplist[element].extend([y])
+        if isinstance(self.type, np.ndarray):
+            for x in self.queue:
+                if self.__nonethreshold(x):
+                    for y in x:
+                        element = list(x).index(y)
+                        tmplist[element].extend([y])
+        else:
+            for x in self.queue:
+                if self.__nonethreshold(x):
+                    for y in (x or []):
+                        element = x.index(y)
+                        tmplist[element].extend([y])
         return tmplist
 
     def average(self):
         if isinstance(self.type, (float, int)):
-            return statistics.mean(self.queue)
-        elif isinstance(self.type, list):
-            tmp = self.second_dimension_list()
+            return mean(self.queue)
+        elif isinstance(self.type, (list, np.ndarray)):
+            tmp = self.__second_dimension_list()
             one_d_list = []
             for x in range(len(tmp)):
                 innertmp = tmp[x]
-                mean = statistics.mean(innertmp)
-                one_d_list.append(mean)
+                newmean = mean(innertmp)
+                one_d_list.append(newmean)
             return one_d_list
         else:
             print("Incompatible data type")
 
     def median(self):
         if isinstance(self.type, (float, int)):
-            return statistics.median(self.queue)
-        elif isinstance(self.type, list):
-            tmp = self.second_dimension_list()
+            return median(self.queue)
+        elif isinstance(self.type, (list, np.ndarray)):
+            tmp = self.__second_dimension_list()
             one_d_list = []
             for x in range(len(tmp)):
                 innertmp = tmp[x]
-                median = statistics.median(innertmp)
-                one_d_list.append(median)
+                innermedian = median(innertmp)
+                one_d_list.append(innermedian)
+            return one_d_list
+        else:
+            print("Incompatible data type")
+
+    def mode(self):
+        if isinstance(self.type, (float, int)):
+            return statistics.mode(self.queue)
+        elif isinstance(self.type, (list, np.ndarray)):
+            tmp = self.__second_dimension_list()
+            one_d_list = []
+            for x in range(len(tmp)):
+                innertmp = tmp[x]
+                mode = statistics.mode(innertmp)
+                one_d_list.append(mode)
             return one_d_list
         else:
             print("Incompatible data type")
