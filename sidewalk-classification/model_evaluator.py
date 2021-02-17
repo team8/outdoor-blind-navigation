bc88fbb592841a3fb20d38522aeb00c5fed58f55
aoberai <aoberai05@gmail.com>
2021-02-17 10:38:15 -0800

    added fps counter

--- sidewalk-classification/model_evaluator.py
+++ sidewalk-classification/model_evaluator.py
@@ -1,6 +1,7 @@
 import cv2
 import tensorflow as tf
 import numpy as np
+import time
 from utils.circularBuffer import CircularBuffer
 
 classes = ['Left of Sidewalk', 'Middle of Sidewalk', 'Right of Sidewalk']
@@ -10,6 +11,7 @@ cb = CircularBuffer(15)
 state = ""
 cb.initQueue([0, 0, 0])
 while True:
+    start_time = time.time()
     orig_cap = cv2.resize(cv2.rotate(vid.read()[1], cv2.ROTATE_90_COUNTERCLOCKWISE), (480, 360))
     cap = cv2.resize(orig_cap, (100, 100))
     raw_prediction = (model.predict(np.expand_dims(cap, 0))).tolist()[0]
@@ -19,7 +21,9 @@ while True:
         state = "Nothing detected"
     else:
         state = classes[np.argmax(averaged_prediction)]
+    end_time = time.time()
+    
     cv2.imshow("Sidewalk Stream", orig_cap)
-    print(state, averaged_prediction)
+    print("FPS:", 1/ (end_time - start_time), state, np.argmax(averaged_prediction))
     cv2.waitKey(1)
 
