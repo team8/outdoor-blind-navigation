7e847c616800bd4d96d8e7273bba4e483f4bdb3f
aoberai <aoberai05@gmail.com>
2021-02-24 10:39:08 -0800

    multithreaded inference, drawing, and capturing/preprocessing. gettting very good fps on video stream using bigger yolov3 model - need to test with yolov4 tiny or perhaps yolov4 because this significantly increased fps

--- src/person-automobile-sign-detection/darknet-default-yolo-fast/custom_darknet.py
+++ src/person-automobile-sign-detection/darknet-default-yolo-fast/custom_darknet.py
@@ -5,8 +5,7 @@ import cv2
 import time
 import darknet
 import argparse
-from threading import Thread, enumerate
-from queue import Queue
+import threading
 
 video_path = 0 #webcam
 weights_path = "./yolov3.weights"
@@ -20,25 +19,68 @@ network, class_names, class_colors = darknet.load_network(
             weights_path,
             batch_size=1
         )
+detections_queue = []
+images_queue = []
 width = darknet.network_width(network)
 height = darknet.network_height(network)
+colors = {"person": [255, 255, 0], "car": [100, 0, 0], "stopsign": [100, 100, 0]}
+
+def capture_processing():
+    while True:
+        try:
+            frame = video_capture.read()[1]
+            preprocessed_frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_LINEAR)
+            darknet_image = darknet.make_image(width, height, 3)
+            darknet.copy_image_from_bytes(darknet_image, preprocessed_frame.tobytes())
+            images_queue.append((darknet_image, preprocessed_frame, time.time()))
+        except Exception as e:
+            print(e)
+
+# def inference():
+#     while True:
+#         try:
+#             last_darknet_image = images_queue[-1][0]
+#             detections = darknet.detect_image(network, class_names, last_darknet_image, thresh=0.25)
+#             detections_queue.append(detections)
+#         except Exception as e:
+#             print(e)
+
+def display():
+    while True:
+        try:
+            last_detection = detections_queue[-1]
+            last_image = images_queue[-1][1]
+            last_time = images_queue[-1][2]
+            for detection in last_detection:
+                print(detection)
+                x, y, w, h = detection[2]
+                xmin = int(x - (w / 2))
+                xmax = int(x + (w / 2))
+                ymin = int(y - (h / 2))
+                ymax = int(y + (h / 2))
+                display_image = cv2.rectangle(last_image, (xmax, ymax), (xmin, ymin), colors[detection[0]] if detection[0] in colors else [0, 100, 0], 4)
+            cv2.imshow("Stream", display_image)
+            cv2.waitKey(1)
+            print("FPS: " + str(1/(time.time() - last_time)))
+
+            # print("FPS: " + str(1/(time.time() - frame_start_time)))
+        except Exception as e:
+                print(e)
 
 while video_capture.isOpened():
-    frame_start_time = time.time()
-    frame = video_capture.read()[1]
-    preprocessed_frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_LINEAR)
-    darknet_image = darknet.make_image(width, height, 3)
-    darknet.copy_image_from_bytes(darknet_image, preprocessed_frame.tobytes())
-    detections = darknet.detect_image(network, class_names, darknet_image, thresh=0.25)
-    for detection in detections:
-        print(detection)
-        x, y, w, h = detection[2]
-        xmin = int(round(x - (w / 2)))
-        xmax = int(round(x + (w / 2)))
-        ymin = int(round(y - (h / 2)))
-        ymax = int(round(y + (h / 2)))
-        preprocessed_frame = cv2.rectangle(preprocessed_frame, (xmax, ymax), (xmin, ymin), (255, 255, 0), 4)
-    print("FPS: " + str(1/(time.time() - frame_start_time)))
-    cv2.imshow("Stream", preprocessed_frame)
-    cv2.waitKey(1)
+    # frame_start_time = time.time()
+    threading.Thread(target=capture_processing).start()
+    # threading.Thread(target=inference).start()
+    threading.Thread(target=display).start()
 
+    while True:
+        try:
+            last_darknet_image = images_queue[-1][0]
+            detections = darknet.detect_image(network, class_names, last_darknet_image, thresh=0.25)
+            detections_queue.append(detections)
+        except Exception as e:
+            print(e)
+
+# Thread(target=video_capture, args=(frame_queue, darknet_image_queue)).start()
+# Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue)).start()
+# Thread(target=drawing, args=(frame_queue, detections_queue, fps_queue)).start()
