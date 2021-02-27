333e53e7156e5eb572fe278b3e2052af7d832d0c
Maxwell938 <71574636+Maxwell938@users.noreply.github.com>
2021-02-26 19:44:04 -0800

    Added code in display.py that boxes 7 different obstacles and labels them(if they exist)

---
+++ src/sidewalk-classification/display.py
@@ -0,0 +1,107 @@
+import pygame
+class Display:
+    def __init__(self,size):
+        pygame.init()
+        self.__image_preprocessing(size)
+
+    def __image_preprocessing(self,size):
+
+        self.font = pygame.font.Font('freesansbold.ttf', 20)
+        self.textSign = self.font.render('Stop Sign', True, (0, 0, 255), None)
+        self.textSignRect = self.textSign.get_rect()
+        self.textHuman = self.font.render('Person', True, (0,255,0), None)
+        self.textHumanRect = self.textHuman.get_rect()
+        self.textCar = self.font.render('Car', True, (255, 0, 0), None)
+        self.textCarRect = self.textCar.get_rect()
+        self.textBike = self.font.render('Bike', True, (255, 255, 0), None)
+        self.textBikeRect = self.textBike.get_rect()
+        self.textLight = self.font.render('Traffic Light', True, (255, 0, 255), None)
+        self.textLightRect = self.textLight.get_rect()
+        self.textHydrant = self.font.render('Fire Hydrant', True, (0, 255, 255), None)
+        self.textHydrantRect = self.textHydrant.get_rect()
+        self.textBench = self.font.render('Bench', True, (200, 100, 200), None)
+        self.textBenchRect = self.textBench.get_rect()
+
+        self.screen = pygame.display.set_mode((size[0],size[1]))
+        self.imgLeft = pygame.image.load("D:/Maxwell/SpecialRobotStuff/blind-navigation/src/display-resources/Right.png")
+        self.imgForward = pygame.image.load("D:/Maxwell/SpecialRobotStuff/blind-navigation/src/display-resources/Forward.png")
+        self.imgRight = pygame.image.load("D:/Maxwell/SpecialRobotStuff/blind-navigation/src/display-resources/Left.png")
+        self.screenSizeXConstant = 1575
+        self.screenSizeYConstant = 1000
+        self.rectVideo = ((0,0,size[0],size[1]))
+
+        self.imgLeft = self.imgLeft.convert_alpha()
+        self.rectLeft = self.imgLeft.get_rect()
+        self.rectLeft[2] /= 3
+        self.rectLeft[3] /= 3
+        self.rectLeft[2] *= self.rectVideo[2] / self.screenSizeXConstant
+        self.rectLeft[3] *= self.rectVideo[3] / self.screenSizeYConstant
+        self.rectLeft = self.rectLeft.move((self.rectVideo[2] / 2 - self.rectLeft[2] / 2 + self.rectVideo[2]/10, 5 / 10 * self.rectVideo[3]))
+        self.imgLeft = pygame.transform.scale(self.imgLeft, (self.rectLeft[2], self.rectLeft[3]))
+
+        self.imgForward = self.imgForward.convert_alpha()
+        self.rectForward = self.imgForward.get_rect()
+        self.rectForward[2] /= 2
+        self.rectForward[3] /= 2
+        self.rectForward[2] *= self.rectVideo[2] / self.screenSizeXConstant
+        self.rectForward[3] *= self.rectVideo[3] / self.screenSizeYConstant
+        self.rectForward = self.rectForward.move((self.rectVideo[2] / 2 - self.rectForward[2]/2, 5/10 * self.rectVideo[3]))
+        self.imgForward = pygame.transform.scale(self.imgForward, (self.rectForward[2],self.rectForward[3]))
+
+        self.imgRight = self.imgRight.convert_alpha()
+        self.rectRight = self.imgRight.get_rect()
+        self.rectRight[2] /= 3
+        self.rectRight[3] /= 3
+        self.rectRight[2] *= self.rectVideo[2] / self.screenSizeXConstant
+        self.rectRight[3] *= self.rectVideo[3] / self.screenSizeYConstant
+        self.rectRight = self.rectRight.move((self.rectVideo[2] / 2 - self.rectRight[2] / 2 - self.rectVideo[2]/10, 5 / 10 * self.rectVideo[3]))
+        self.imgRight = pygame.transform.scale(self.imgRight, (self.rectRight[2], self.rectRight[3]))
+    def update(self,state, orig_cap, obstacles):
+        orig_cap = orig_cap.swapaxes(0, 1)
+        orig_cap = orig_cap[:, :, ::-1]
+        pygame.surfarray.blit_array(self.screen, orig_cap)
+        if state == "Left of Sidewalk":
+            self.screen.blit(self.imgLeft, self.rectLeft)
+        elif state == "Right of Sidewalk":
+            self.screen.blit(self.imgRight, self.rectRight)
+        elif state == "Middle of Sidewalk":
+            self.screen.blit(self.imgForward, self.rectForward)
+        for size in range(len(obstacles)):
+            self.displayObjects(obstacles[size])
+        pygame.display.update()
+    def displayObjects(self, RectCords):
+        empty_rect = pygame.Rect(RectCords[1], RectCords[2], RectCords[3]-RectCords[1], RectCords[4]-RectCords[2])
+        centerX = (RectCords[1] + RectCords[3])/2
+        centerY = max(RectCords[2],RectCords[4])+15
+        if (centerY + 15 >= 360):
+            centerY = min(RectCords[2],RectCords[4])-15
+
+        if (RectCords[0] == 'Stop Sign'):
+            self.textSignRect.center = (centerX,centerY)
+            self.screen.blit(self.textSign, self.textSignRect)
+            pygame.draw.rect(self.screen, (0, 0, 255), empty_rect, 3)
+        elif (RectCords[0] == 'Car'):
+            self.textCarRect.center = (centerX,centerY)
+            self.screen.blit(self.textCar, self.textCarRect)
+            pygame.draw.rect(self.screen, (255, 0, 0), empty_rect, 3)
+        elif (RectCords[0] == 'Bike'):
+            self.textBikeRect.center = (centerX,centerY)
+            self.screen.blit(self.textBike, self.textBikeRect)
+            pygame.draw.rect(self.screen, (255, 255, 0), empty_rect, 3)
+        elif (RectCords[0] == 'Person'):
+            self.textHumanRect.center = (centerX,centerY)
+            self.screen.blit(self.textHuman, self.textHumanRect)
+            pygame.draw.rect(self.screen, (0, 255, 0), empty_rect, 3)
+        elif (RectCords[0] == 'Bench'):
+            self.textBenchRect.center = (centerX,centerY)
+            self.screen.blit(self.textBench, self.textBenchRect)
+            pygame.draw.rect(self.screen, (200, 100, 200), empty_rect, 3)
+        elif (RectCords[0] == 'Fire Hydrant'):
+            self.textHydrantRect.center = (centerX,centerY)
+            self.screen.blit(self.textHydrant, self.textHydrantRect)
+            pygame.draw.rect(self.screen, (0, 255, 255), empty_rect, 3)
+        if (RectCords[0] == 'Traffic Light'):
+            self.textLightRect.center = (centerX,centerY)
+            self.screen.blit(self.textLight, self.textLightRect)
+            pygame.draw.rect(self.screen, (255, 0, 255), empty_rect, 3)
+
--- src/sidewalk-classification/model_evaluator.py
+++ src/sidewalk-classification/model_evaluator.py
@@ -1,11 +1,11 @@
 import cv2
-import tensorflow as tf
+#import tensorflow as tf
 import time
 from utils.circularBuffer import CircularBuffer
 from display import Display
 import numpy as np
 classes = ['Left of Sidewalk', 'Middle of Sidewalk', 'Right of Sidewalk']
-model = tf.keras.models.load_model("sidewalk_classification_model_resnet.h5")
+#model = tf.keras.models.load_model("sidewalk_classification_model_resnet.h5")
 # vid = cv2.VideoCapture("/home/aoberai/Downloads/Long_Sidewalk.mp4")
 rotate = False
 vid = cv2.VideoCapture(0)
@@ -20,13 +20,15 @@ if vid.read()[0] is False:
     vid.set(cv2.CAP_PROP_POS_FRAMES, (position_video * vid.get(cv2.CAP_PROP_FRAME_COUNT)))
 
 
-cb = CircularBuffer(20, minNumPercent = 0.8)
+#cb = CircularBuffer(20, minNumPercent = 0.8)
 state = ""
 sidewalk_display = Display((480,360))
 print(1)
+obs = ((("Person",200,200,10,10),("Traffic Light",250,250,360,360)))
 while True:
     start_time = time.time()
     orig_cap = cv2.resize(cv2.rotate(vid.read()[1], cv2.ROTATE_90_COUNTERCLOCKWISE) if rotate == True else vid.read()[1], (480, 360))
+    """
     cap = cv2.resize(orig_cap, (100, 100))
     raw_prediction = (model.predict(np.expand_dims(cap, 0))).tolist()[0]
     cb.add(None if max(raw_prediction) < 0.8 else raw_prediction)
@@ -38,6 +40,7 @@ while True:
     end_time = time.time()
     confidence =  max(averaged_prediction) * 100 // 1 if averaged_prediction is not None else None
     print("FPS:", 1/ (end_time - start_time), state, "Confidence:", confidence)
-    sidewalk_display.update(state, orig_cap)
+    """
+    sidewalk_display.update(state, orig_cap,obs)
 
 #runfile("D:/Maxwell/SpecialRobotStuff/blind-navigation/src/sidewalk-classification/model_evaluator.py")
