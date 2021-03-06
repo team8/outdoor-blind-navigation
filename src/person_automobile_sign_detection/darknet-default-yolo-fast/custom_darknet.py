7d5804ba80cf7dcfc522841f145eb2385a1fe48f
aoberai <aoberai05@gmail.com>
2021-03-05 16:09:04 -0800

    changed folder naming convention

---
+++ src/person_automobile_sign_detection/darknet-default-yolo-fast/custom_darknet.py
@@ -0,0 +1,96 @@
+from ctypes import *
+import random
+import os
+import cv2
+import time
+import darknet
+import argparse
+import threading
+from utils.circularBuffer import CircularBuffer
+
+video_path = 0 #webcam
+weights_path = "./yolov4.weights"
+config_path = "./yolov4.cfg"
+data_file_path = "./coco.data"
+
+video_capture = cv2.VideoCapture(video_path)
+network, class_names, class_colors = darknet.load_network(
+            config_path,
+            data_file_path,
+            weights_path,
+            batch_size=1
+        )
+detections_queue = []
+images_queue = []
+fps_queue = [0] 
+width = darknet.network_width(network)
+height = darknet.network_height(network)
+colors = {"person": [255, 255, 0], "car": [100, 0, 0], "stopsign": [100, 100, 0]}
+detections_average = CircularBuffer(5)
+
+def capture_processing():
+    while True:
+        try:
+            frame = video_capture.read()[1]
+            preprocessed_frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_LINEAR)
+            darknet_image = darknet.make_image(width, height, 3)
+            darknet.copy_image_from_bytes(darknet_image, preprocessed_frame.tobytes())
+            images_queue.append((darknet_image, preprocessed_frame))
+        except Exception as e:
+            print(e)
+
+# def inference():
+#     while True:
+#         try:
+#             last_darknet_image = images_queue[-1][0]
+#             detections = darknet.detect_image(network, class_names, last_darknet_image, thresh=0.25)
+#             detections_queue.append(detections)
+#         except Exception as e:
+#             print(e)
+
+def display():
+    while True:
+        try:
+            last_detection = detections_queue[-1]
+            last_image = images_queue[-1][1]
+            fps = fps_queue[-1]
+            for detection in last_detection:
+                # print(detection)
+                x, y, w, h = detection[2]
+                xmin = int(x - (w / 2))
+                xmax = int(x + (w / 2))
+                ymin = int(y - (h / 2))
+                ymax = int(y + (h / 2))
+                detections_average.add([xmax, ymax, xmin, ymin]) #this is wrong - should only do this for individual objects. this is just merging everything together (very bad)
+                detections_mean = detections_average.mean()
+
+                print(detection[0] + " FPS: " + str(fps))
+                if detections_mean is not None:
+                    display_image = cv2.rectangle(last_image, (int(detections_mean[0]), int(detections_mean[1])), (int(detections_mean[2]), int(detections_mean[3])), colors[detection[0]] if detection[0] in colors else [0, 100, 0], 4)
+            cv2.imshow("Stream", display_image)
+            cv2.waitKey(1)
+
+            # print("FPS: " + str(1/(time.time() - frame_start_time)))
+        except Exception as e:
+                print(e)
+
+while video_capture.isOpened():
+    # frame_start_time = time.time()
+    threading.Thread(target=capture_processing).start()
+    # threading.Thread(target=inference).start()
+    threading.Thread(target=display).start()
+
+    while True:
+        try:
+            last_darknet_image = images_queue[-1][0]
+            last_time = time.time()
+            detections = darknet.detect_image(network, class_names, last_darknet_image, thresh=0.25)
+            detections_queue.append(detections)
+
+            fps_queue.append(1/(time.time() - last_time))
+        except Exception as e:
+            print(e)
+
+# Thread(target=video_capture, args=(frame_queue, darknet_image_queue)).start()
+# Thread(target=inference, args=(darknet_image_queue, detections_queue, fps_queue)).start()
+# Thread(target=drawing, args=(frame_queue, detections_queue, fps_queue)).start()
---
+++ src/person_automobile_sign_detection/darknet-default-yolo-fast/utils/circularBuffer.py
@@ -0,0 +1,82 @@
+import statistics
+import numpy as np
+
+#Using an inefficient but easy to code implementation
+class CircularBuffer:
+    def __init__(self, capacity, noneOverridePercent =  0.5):
+        self.capacity = capacity
+        self.queue = [None] * capacity
+        self.minNumPercent = noneOverridePercent
+        self.lastAccessed = False
+
+    #Start out the buffer with given list listIn
+    def initQueue(self, listIn):
+        temp = listIn.copy()
+        temp.reverse()
+        for i in temp:
+            self.add(i)
+
+    #Push a new item onto the buffer
+    def add(self, term):
+        del self.queue[-1]
+        self.queue.insert(0, term)
+        self.lastAccessed = False
+
+    #Writes out everything in the circularBuffer
+    def toString(self):
+        strOut = ""
+        for i in self.queue:
+            strOut += str(i)
+        return strOut
+
+    #Averages everything in the queue (can't take strings etc). If number of nones is greater than minNumPercent, it returns None
+    def mean(self):
+        temp = self.queue.copy()
+        if temp.count(None) >= self.minNumPercent * self.capacity:
+            return None
+        else:
+            temp = list(filter(None, temp))
+        if(type(temp[0]) == list):
+            return np.mean(temp, axis=0)
+        else:
+            temp = list(filter(None, temp))
+            return sum(temp) / len(temp)
+
+    #Sorts a copy of the buffer, then finds the median. Probably doesn't work on strings. If greater than minNumPercent, returns None
+    def median(self):
+        temp = self.queue.copy()
+        if temp.count(None) >= self.minNumPercent * self.capacity:
+            return None
+        else:
+            temp = list(filter(None, temp))
+            temp.sort()
+            if(self.capacity % 2 == 0):
+                return temp[self.capacity // 2 + 1] + [self.capacity // 2] / 2
+            return temp[self.capacity // 2 + 1]
+
+    #Finds the thing with the most instances in the lisdt. Same as other 2, returns none if over minNumPercent
+    def mode(self):
+        temp = self.queue.copy()
+        if temp.count(None) >= self.minNumPercent * self.capacity:
+            return None
+        maxOccurrences = 0
+        modeOut = None
+        for i in temp:
+            totalOfInstance = temp.count(i)
+            if totalOfInstance > maxOccurrences:
+                modeOut = i
+                maxOccurrences = totalOfInstance
+        return modeOut
+
+    def size(self):
+        return len(self.queue)
+
+    def getLast(self):
+        self.lastAccessed = True
+        return self.queue[-1]
+
+    def getList(self):
+        return self.queue
+
+    def getLastAccessed(self) -> bool:
+        return self.lastAccessed
