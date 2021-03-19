505cc351686007a75cd194d73e22a939f91c3c81
Maxwell938 <71574636+Maxwell938@users.noreply.github.com>
2021-03-19 16:01:23 -0700

    changed the scaling for x and y

--- src/display_tester.py
+++ src/display_tester.py
@@ -1,13 +1,13 @@
-import cv2
-import time
+
 from display import Display
+import cv2
+display = Display()
 
-display = Display((416,416))
+video = cv2.VideoCapture(0)
+capture = video.read(0)[1]
 
-capture = cv2.imread("./display_resources/Carl.png")
+counter = 0
 obs = [('person', '30.26', (250.02488708496094, 85.19881439208984, 62.963951110839844, 129.86805725097656)), ('traffic light', '34.59', (246.94873046875, 83.70958709716797, 59.21462631225586, 127.32121276855469))]
-while True:
-    display.putVideoFeed(capture)
-    display.putObjects(obs)
-    display.displayScreen()
-    time.sleep(1)
+display.putVideoFeed(capture)
+display.putObjects(obs)
+display.displayScreen()
