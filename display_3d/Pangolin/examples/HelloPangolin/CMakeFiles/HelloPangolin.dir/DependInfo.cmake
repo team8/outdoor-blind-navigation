d5866bcfc48616c2e625509b9f7acf27baeb29ab
aoberai <aoberai05@gmail.com>
2021-04-06 21:46:50 -0700

    displaying something on to the cube - not even close to what i want however

---
+++ display_3d/Pangolin/examples/HelloPangolin/CMakeFiles/HelloPangolin.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aoberai/programming/python/blind-navigation/display_3d/Pangolin/examples/HelloPangolin/main.cpp" "/home/aoberai/programming/python/blind-navigation/display_3d/Pangolin/examples/HelloPangolin/CMakeFiles/HelloPangolin.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/aoberai/programming/python/blind-navigation/display_3d/Pangolin/test/Pangolin/include"
+  "/home/aoberai/programming/python/blind-navigation/display_3d/Pangolin/test/Pangolin/build/src/include"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- display_3d/SimpleDisplay.py
+++ display_3d/SimpleDisplay.py
@@ -1,18 +1,14 @@
-
+import pygame
 import pypangolin as pango
 from OpenGL.GL import *
-
-
-def a_callback():
-    print("a pressed")
-
+import cv2
 
 def main():
     win = pango.CreateWindowAndBind("pySimpleDisplay", 640, 480)
     glEnable(GL_DEPTH_TEST)
 
 
-   # Define Projection and initial ModelView matrix
+    # Define Projection and initial ModelView matrix
 
     pm = pango.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.1, 1000)
     mv = pango.ModelViewLookAt(-0, 0.5, -3, 0, 0, 0, pango.AxisY)
@@ -34,34 +30,113 @@ def main():
         .SetHandler(handler)
     )
 
-    pango.CreatePanel("ui").SetBounds(
-        pango.Attach(0), pango.Attach(1), pango.Attach(0), pango.Attach.Pix(ui_width)
-    )
-    var_ui = pango.Var("ui")
-    var_ui.a_Button = False
-    var_ui.a_double = (0.0, pango.VarMeta(0, 5))
-    var_ui.an_int = (2, pango.VarMeta(0, 5))
-    var_ui.a_double_log = (3.0, pango.VarMeta(1, 1e4, logscale=True))
-    var_ui.a_checkbox = (False, pango.VarMeta(toggle=True))
-    var_ui.an_int_no_input = 2
-    var_ui.a_str = "sss"
+    # pango.CreatePanel("ui").SetBounds(
+    # pango.Attach(0), pango.Attach(1), pango.Attach(0), pango.Attach.Pix(ui_width)
+    # )
+    # var_ui = pango.Var("ui")
+    # var_ui.a_Button = False
+    # var_ui.a_double = (0.0, pango.VarMeta(0, 5))
+    # var_ui.an_int = (2, pango.VarMeta(0, 5))
+    # var_ui.a_double_log = (3.0, pango.VarMeta(1, 1e4, logscale=True))
+    # var_ui.a_checkbox = (False, pango.VarMeta(toggle=True))
+    # var_ui.an_int_no_input = 2
+    # var_ui.a_str = "sss"
+    #
+    # ctrl = -96
+    # pango.RegisterKeyPressCallback(ctrl + ord("a"), a_callback)
 
-    ctrl = -96
-    pango.RegisterKeyPressCallback(ctrl + ord("a"), a_callback)
+    # textureSurface = pygame.image.load('test_image.png')
+    # textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
+    # width = textureSurface.get_width()
+    # height = textureSurface.get_height()
+    vid = cv2.VideoCapture("/home/aoberai/Downloads/Long_Sidewalk_Compressed.mp4")
+    textureData = vid.read()[1]
+    glEnable(GL_TEXTURE_2D)
+    texid = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texid)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureData.shape[0], textureData.shape[1],
+                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
 
     while not pango.ShouldQuit():
         # Clear screen and activate view to render into
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        if var_ui.a_checkbox:
-            var_ui.an_int = var_ui.a_double
-
-        var_ui.an_int_no_input = var_ui.an_int
+        # if var_ui.a_checkbox:
+        # var_ui.an_int = var_ui.a_double
+        #
+        # var_ui.an_int_no_input = var_ui.an_int
 
         d_cam.Activate(s_cam)
+        cubeVertices = ((1,1,1),(1,1,-1),(1,-1,-1),(1,-1,1),(-1,1,1),(-1,-1,-1),(-1,-1,1),(-1, 1,-1))
+        cubeEdges = ((0,1),(0,3),(0,4),(1,2),(1,7),(2,5),(2,3),(3,6),(4,6),(4,7),(5,6),(5,7))
+        cubeQuads = ((0,3,6,4),(2,5,6,3),(1,2,5,7),(1,0,4,7),(7,4,6,5),(2,3,0,1))
 
+
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0,  1.0,  1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(1.0,  1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0, -1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0,  1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0,  1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(1.0, -1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0,  1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(1.0,  1.0,  1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0, -1.0,  1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glEnd()
+        # glBegin(GL_LINES)
+        # for cubeEdge in cubeEdges:
+        #     for cubeVertex in cubeEdge:
+        #         glVertex3fv(cubeVertices[cubeVertex])
+        # glEnd()
+
+        glColor3f(1.0,1.0,1.0)
         # Render OpenGL Cube
-        pango.glDrawColouredCube()
+        # pango.glDrawColouredCube(5)
 
         # Swap Frames and Process Events
         pango.FinishFrame()
