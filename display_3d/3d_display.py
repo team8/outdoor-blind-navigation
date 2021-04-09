f7e4af22fcef7634638ff9f5b3cc4ecf7bace8b9
aoberai <aoberai05@gmail.com>
2021-04-09 01:02:59 -0700

    added more comments for figuring stuff out, changed a bunch of camera params, figured out how to change s cam position for 3d scroll using left button

--- display_3d/3d_display.py
+++ display_3d/3d_display.py
@@ -10,30 +10,31 @@ def main():
 
     # Define Projection and initial ModelView matrix
 
-    # last param is like movement speed, find second last parameter meaning
-    pm = pango.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100)
+
+    #   ProjectionMatrix (int w, int h, double fu, double fv, double u0, double v0, double zNear, double zFar)
+    pm = pango.ProjectionMatrix(640*2, 480*2, 420, 420, 320, 240, 0.5, 100)
 
     # This allows changing of "camera" angle : glulookat style model view matrix (x, y, z, lx, ly, lz, AxisDirection Up) Forward is -z and up is +y
-    mv = pango.ModelViewLookAt(-3, 0, -8,
-                             0, 0, 0,
-                             0, -1, 0)
-    # mv = pango.ModelViewLookAt(10, 10, 20,
+
+    # Left click drag to move orientation relative to object
+    mv = pango.ModelViewLookAt(-3, 0, -10,
+                                0, 0, 0,
+                             0, -1, 0) # inference that last row has something to do with scaling or coord system type? that would make sense for why it can be replced with pango.AxisY
+    # mv = pango.ModelViewLookAt(10, 10, 50,
                              # 0, 0, 0,
                              # 0, -1, 0)
     # This is normal view of object
-    # mv = pango.ModelViewLookAt(-0, 0.05, -3, 0, 0, 0, pango.AxisY) ## TODO: what is axis y and axis x
+    # mv = pango.ModelViewLookAt(-0, 0.05, -3, 0, 0, 0, pango.AxisY) # TODO: what is axis y and axis x
     s_cam = pango.OpenGlRenderState(pm, mv)
 
-    ui_width = 180
     # Create Interactive View in window
-
     handler = pango.Handler3D(s_cam)
     d_cam = (
         pango.CreateDisplay()
         .SetBounds(
             pango.Attach(0),
             pango.Attach(1),
-            pango.Attach.Pix(ui_width),
+            pango.Attach.Pix(1), # side bar which can be used for notification system
             pango.Attach(1),
             -640.0 / 480.0,
         )
@@ -46,10 +47,10 @@ def main():
     # var_ui = pango.Var("ui")
     # var_ui.a_Button = False
     # var_ui.a_double = (0.0, pango.VarMeta(0, 5))
-    # var_ui.an_int = (2, pango.VarMeta(0, 5))
+    # var_ui.an_int = (5, pango.VarMeta(0, 5))
     # var_ui.a_double_log = (3.0, pango.VarMeta(1, 1e4, logscale=True))
     # var_ui.a_checkbox = (False, pango.VarMeta(toggle=True))
-    # var_ui.an_int_no_input = 2
+    # var_ui.an_int_no_input = 5
     # var_ui.a_str = "sss"
     #
     # ctrl = -96
@@ -59,9 +60,9 @@ def main():
     textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
     width = textureSurface.get_width()
     height = textureSurface.get_height()
-    # vid = cv2.VideoCapture("/home/aoberai/Downloads/Long_Sidewalk_Compressed.mp4")
+    # vid = cv5.VideoCapture("/home/aoberai/Downloads/Long_Sidewalk_Compressed.mp4")
     # textureData = vid.read()[1]
-    # textureData = cv2.resize(cv2.imread("test_image.jpg"), (500, 500))
+    # textureData = cv5.resize(cv5.imread("test_image.jpg"), (500, 500))
     glEnable(GL_TEXTURE_2D)
     texid = glGenTextures(1)
 
@@ -85,8 +86,8 @@ def main():
 
         d_cam.Activate(s_cam)
         cubeVertices = ((1,1,1),(1,1,-1),(1,-1,-1),(1,-1,1),(-1,1,1),(-1,-1,-1),(-1,-1,1),(-1, 1,-1))
-        cubeEdges = ((0.05),(0,3),(0,4),(1,2),(1,7),(2,5),(2,3),(3,6),(4,6),(4,7),(5,6),(5,7))
-        cubeQuads = ((0,3,6,4),(2,5,6,3),(1,2,5,7),(1,0,4,7),(7,4,6,5),(2,3,0.05))
+        cubeEdges = ((0.01),(0,3),(0,4),(1,5),(1,7),(5,5),(5,3),(3,6),(4,6),(4,7),(5,6),(5,7))
+        cubeQuads = ((0,3,6,4),(5,5,6,3),(1,5,5,7),(1,0,4,7),(7,4,6,5),(5,3,0.01))
 
 
         glBegin(GL_QUADS)
@@ -94,53 +95,53 @@ def main():
         #     for cubeVertex in cubeQuad:
         #         glVertex3fv(cubeVertices[cubeVertex])
         # glTexCoord2f(0.0, 0.0)
-        glVertex3f(-2, -2, 0.05)
-        # glTexCoord2f(2, 0.0)
-        glVertex3f(2, -2,  0.05)
-        # glTexCoord2f(2, 2)
-        glVertex3f(2,  2,  0.05)
-        # glTexCoord2f(0.0, 2)
-        glVertex3f(-2,  2, 0.05)
-        glTexCoord2f(2, 0.0)
-        glVertex3f(-2, -2, -0.05)
-        glTexCoord2f(2, 2)
-        glVertex3f(-2,  2, -0.05)
-        glTexCoord2f(0.0, 2)
-        glVertex3f(2,  2, -0.05)
+        glVertex3f(-5, -5, 0.01)
+        # glTexCoord2f(5, 0.0)
+        glVertex3f(5, -5,  0.01)
+        # glTexCoord5f(5, 5)
+        glVertex3f(5,  5,  0.01)
+        # glTexCoord5f(0.0, 5)
+        glVertex3f(-5,  5, 0.01)
+        glTexCoord2f(5, 0.0)
+        glVertex3f(-5, -5, -0.01)
+        glTexCoord2f(5, 5)
+        glVertex3f(-5,  5, -0.01)
+        glTexCoord2f(0.0, 5)
+        glVertex3f(5,  5, -0.01)
         glTexCoord2f(0.0, 0.0)
-        glVertex3f(2, -2, -0.05)
-        # glTexCoord2f(0.0, 2)
-        glVertex3f(-2,  2, -0.05)
-        # glTexCoord2f(0.0, 0.0)
-        glVertex3f(-2,  2,  0.05)
-        # glTexCoord2f(2, 0.0)
-        glVertex3f(2,  2,  0.05)
-        # glTexCoord2f(2, 2)
-        glVertex3f(2,  2, -0.05)
-        # glTexCoord2f(2, 2)
-        glVertex3f(-2, -2, -0.05)
-        # glTexCoord2f(0.0, 2)
-        glVertex3f(2, -2, -0.05)
-        # glTexCoord2f(0.0, 0.0)
-        glVertex3f(2, -2, 0.05)
-        # glTexCoord2f(2, 0.0)
-        glVertex3f(-2, -2, 0.05)
-        # glTexCoord2f(2, 0.0)
-        glVertex3f(2, -2, -0.05)
-        # glTexCoord2f(2, 2)
-        glVertex3f(2,  2, -0.05)
-        # glTexCoord2f(0.0, 2)
-        glVertex3f(2,  2, 0.05)
-        # glTexCoord2f(0.0, 0.0)
-        glVertex3f(2, -2, 0.05)
-        # glTexCoord2f(0.0, 0.0)
-        glVertex3f(-2, -2, -0.05)
-        # glTexCoord2f(2, 0.0)
-        glVertex3f(-2, -2, 0.05)
-        # glTexCoord2f(2, 2)
-        glVertex3f(-2,  2, 0.05)
-        # glTexCoord2f(0.0, 2)
-        glVertex3f(-2,  2, -0.05)
+        glVertex3f(5, -5, -0.01)
+        # glTexCoord5f(0.0, 5)
+        glVertex3f(-5,  5, -0.01)
+        # glTexCoord5f(0.0, 0.0)
+        glVertex3f(-5,  5,  0.01)
+        # glTexCoord5f(5, 0.0)
+        glVertex3f(5,  5,  0.01)
+        # glTexCoord5f(5, 5)
+        glVertex3f(5,  5, -0.01)
+        # glTexCoord5f(5, 5)
+        glVertex3f(-5, -5, -0.01)
+        # glTexCoord5f(0.0, 5)
+        glVertex3f(5, -5, -0.01)
+        # glTexCoord5f(0.0, 0.0)
+        glVertex3f(5, -5, 0.01)
+        # glTexCoord5f(5, 0.0)
+        glVertex3f(-5, -5, 0.01)
+        # glTexCoord5f(5, 0.0)
+        glVertex3f(5, -5, -0.01)
+        # glTexCoord5f(5, 5)
+        glVertex3f(5,  5, -0.01)
+        # glTexCoord5f(0.0, 5)
+        glVertex3f(5,  5, 0.01)
+        # glTexCoord5f(0.0, 0.0)
+        glVertex3f(5, -5, 0.01)
+        # glTexCoord5f(0.0, 0.0)
+        glVertex3f(-5, -5, -0.01)
+        # glTexCoord5f(5, 0.0)
+        glVertex3f(-5, -5, 0.01)
+        # glTexCoord5f(5, 5)
+        glVertex3f(-5,  5, 0.01)
+        # glTexCoord5f(0.0, 5)
+        glVertex3f(-5,  5, -0.01)
         glEnd()
         # glBegin(GL_LINES)
         # for cubeEdge in cubeEdges:
@@ -148,7 +149,7 @@ def main():
         #         glVertex3fv(cubeVertices[cubeVertex])
         # glEnd()
 
-        # glColor3f(2.05.0.05.0)
+        # glColor3f(5.05.0.01.0)
         # Render OpenGL Cube
         # pango.glDrawColouredCube(5)
 
