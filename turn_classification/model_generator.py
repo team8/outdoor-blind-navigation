c4bb0412afa6939328e52340e59352a0c633926c
aoberai <aoberai05@gmail.com>
2021-06-01 15:03:41 -0700

    added architecture for turn classiication

--- turn_classification/model_generator.py
+++ turn_classification/model_generator.py
@@ -4,11 +4,9 @@ import cv2
 import os
 import numpy as np
 import random
-import preprocessing
-input_shape = (100, 100, 3)
 
-# model.compile(
-    # optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+input_shape = (100, 100, 3)
+labels = ['Turn Left', 'No Turn', 'Turn Right']
 
 VGG16_MODEL=tf.keras.applications.VGG16(input_shape=input_shape,
                                                # include_top=False,
@@ -32,21 +30,13 @@ model.compile(
   optimizer=tf.keras.optimizers.Adam(),
   loss='categorical_crossentropy',
   metrics=['acc'])
-#find best optimizer (Adam, rmsprop, ADAsgrad etc), loss function is binary closs entropy for now because only binary output but try multi-class cross-entropy loss
 
-# important note: if your true labels are one hot encoded ie. [1, 0, 0], [0, 1, 0], use categorical crossentropy, if true labels are integers ie [1], [2], [3], use sparse categorical_srossentropy.  (saves time in computation and memory because uses single integer for a class)
-
-# important note 2: use binary cross entropy when doing multi label classification (possibly inclusive classes) (categorical cross entropy is when you have exclusive classes) (i need binary cross entropy)
-
-
-# model.fit(x=np.array(fire_detection_train_dataset), y=one_hot_encoded_array, epochs=8)
 batch_size = 32
-dataset_path = "../../../ml-datasets/Sidewalk Dataset Final/Sidewalk Classification"
+dataset_path = "../../../ml-datasets/Sidewalk Dataset Final/Turn Dataset/"
 # train_ds = tf.keras.preprocessing.image_dataset_from_directory( dataset_path , validation_split = 0.1, subset="training", seed=123, image_size = (input_shape[0], input_shape[1]))
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(dataset_path , labels='inferred', batch_size=batch_size, validation_split = 0.1, subset="training", seed=121, image_size = (input_shape[0], input_shape[1]), label_mode="categorical")
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(dataset_path , labels='inferred', batch_size=batch_size, validation_split = 0.1, subset="validation", seed=121, image_size = (input_shape[0], input_shape[1]), label_mode="categorical")
-labels = ["Left of Sidewalk", "Middle of Sidewalk", "Right of Sidewalk"]
 
 data_augmentation = tf.keras.Sequential([tf.keras.layers.experimental.preprocessing.RandomRotation((-0.02, 0.02))]) # TODO: Add random contrast
 normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
@@ -59,7 +49,7 @@ model.fit(train_ds, validation_data=val_ds, epochs=8)
 
 print(model.summary())
 if input("Do you want to save model? y for yes, n for no?\n") == 'y':
-    model.save("sidewalk_classification_model_resnet_final.h5")
+    model.save("turn_classification_model_resnet_final.h5")
 
 
 # modifications for 1 input channel for image
---
+++ turn_classification/turn_classification.py
@@ -0,0 +1,52 @@
+import cv2
+import threading
+import tensorflow as tf
+import numpy as np
+import time
+import capturer
+from utils.circularBuffer import CircularBuffer
+
+labels = ['Turn Left', 'No Turn', 'Turn Right']
+model_path = "./turn_classification/turn_classification_model_vgg16_final.h5"
+readings_buffer_size = 25
+image_preprocessing_dimens = (100, 100)
+detection_threshold = 0.5
+
+class TurnClassification:
+
+    def __init__(self):
+        self.model = tf.keras.models.load_model(model_path)
+        self.readings_buffer = CircularBuffer(readings_buffer_size, noneOverridePercent=0.5)
+        self.images_queue = CircularBuffer(1)
+        self.classifier_queue = CircularBuffer(1)
+        threading.Thread(target=self.classification_starter).start()
+
+    def capture_processing(self):
+        while True:
+            try:
+                frame = capturer.getImages().getLast()
+                if frame is not None:
+                    preprocessed_frame = cv2.resize(frame, image_preprocessing_dimens, interpolation=cv2.INTER_LINEAR)
+                    self.images_queue.add(np.expand_dims(preprocessed_frame, 0))
+            except Exception as e:
+                print("Capturing Not Working", e)
+
+    def classification_starter(self):
+        threading.Thread(target=self.capture_processing).start()
+        while True:
+            try:
+                self.perform_inference(self.images_queue.getLast())
+            except Exception as e:
+                print("Classification Not Working", e)
+
+
+    def perform_inference(self, image):
+        feedforward_result = self.model.predict(image).tolist()[0]
+
+        self.readings_buffer.add(None if feedforward_result == None or max(feedforward_result) < detection_threshold else feedforward_result)
+        averaged_result = self.readings_buffer.mean()
+        self.classifier_queue.add(classes[len(classes) - 1] if averaged_result is None else classes[np.argmax(averaged_result)])
+
+    def get_inference(self):
+        return self.classifier_queue.getLast()
+
