e00603332b5155e6c6f98c9fbc0158cd0db397bd
aoberai <aoberai05@gmail.com>
2021-06-04 19:21:54 -0700

    added audio clip integration with non reptitive clip playing and priority queue system for picking audio clips

--- feedback.py
+++ feedback.py
@@ -1,3 +1,6 @@
+from utils.circularBuffer import CircularBuffer
+from feedback import audio_player
+
 def interpret_status(state_classifier_inference, object_localizer_inference):
     status = {"person": False, "stop sign": False, "car": False, "turn left": False, "turn right": False, "shift right": False, "shift left": False, "person collision": False, "car collision": False}
     if state_classifier_inference == "Left of Sidewalk":
@@ -16,3 +19,39 @@ def interpret_status(state_classifier_inference, object_localizer_inference):
             if obstacle["colliding"] == True:
                 status["car collision"] = True
     return status
+
+'''
+For audio:
+
+Create identifier string using id + label or sidewalk state
+Push identifier strings to circular buffer as they come in (making sure only unique identifier strings are on the buffer list
+Play each audio queue based on priority queue order and then pop from list
+This handles making sure multiple not played at the same time
+'''
+
+class AudioFeedback:
+    playedAudioClips = [None] * 10
+    def update(self, state_classifier_inference, object_localizer_inference):
+        for obstacle in object_localizer_inference:
+            identifier = obstacle["label"] + " " + obstacle["id"] + " " + obstacle["colliding"]
+            if identifier not in self.audioClips.getList():
+                self.audioClips.insert(0, identifier)
+
+        if state_classifier_inference == "Left of Sidewalk":
+           print("Add shift right to audio queue")
+           audio_player.runShiftRight()
+        elif state_classifier_inference == "Right of Sidewalk":
+           print("Add shift left to audio queue")
+           audio_player.runShiftLeft()
+        for obstacle in object_localizer_inference:
+            if obstacle["label"] == "stop sign":
+                audio_player.runStopSignDetected(obstacle["id"])
+            if obstacle["label"] == "person":
+                if obstacle["colliding"] == True:
+                   audio_player.runPersonCollisionDetected(obstacle["id"], obstacle["colliding"])
+            if obstacle["label"] == "car":
+                if obstacle["colliding"] == True:
+                    print("Car collision detected")
+                    audio_player.runCarCollisionDetected(obstacle["id"], obstacle["colliding"])
+
+
--- feedback/audio_player.py
+++ feedback/audio_player.py
@@ -1,45 +1,130 @@
 import simpleaudio as sa
+from utils.circularBuffer import CircularBuffer
 
-audioClipDictionary = {
-    "personahead.wav": 1,
-    "personleft.wav": 2,
-    "personright.wav": 3,
-    "stopsignmiddle.wav": 4,
-    "stopsignleft.wav": 5,
-    "stopsignright.wav": 6,
-}
+clipPriority = {"ShiftRight": 0, "ShiftLeft": 1, "Stop": 2, "Person": 3, "Car": 4} # lower value -> higher priority
+wantedAudioClips = []
 
-audioClips = []
-wave_object = None
-play_object = None
-is_playing = False
+def runShiftRight():
+    wantedAudioClips.append("ShiftRight")
+def runShiftLeft():
+    wantedAudioClips.append("ShiftLeft")
+def runStopSignDetected(numid):
+    wantedAudioClips.append("Stop" + " " + str(numid))
+def runPersonCollisionDetected(numid, colliding): #TODO: Make separate for collision from left and right side
+    wantedAudioClips.append("Person" + " " + str(numid) + " " + str(colliding))
+def runCarCollisionDetected(numid, colliding):
+    wantedAudioClips.append("Car" + " " + str(numid) + " " + str(colliding))
+
+class AudioPlayer:
+    playedAudioClips = CircularBuffer(15)
+    def update(self):
+        global wantedAudioClips
+        highestPriorityString = None
+        highestPriorityIndex = None
+        # Find clip with the highest priority
+        for clip in wantedAudioClips:
+            currentClipPriority = clipPriority[clip.split()[0]]
+            if (highestPriorityIndex is None or currentClipPriority < highestPriorityIndex) and highestPriorityString not in self.playedAudioClips.getList():
+                highestPriorityIndex = currentClipPriority
+                highestPriorityString = clip
+        if highestPriorityString is not None:
+            self.playedAudioClips.add(highestPriorityString)
+            self.__play(highestPriorityString)
+
+    def __play(self, audio_string):
+        # Play audio string based on identifier string
+        print("Playing " + audio_string)
+        audio_params = audio_string.split()
+        if audio_params[0] == "ShiftLeft":
+           print("Playing shift left")
+        elif audio_params[0] == "ShiftRight":
+           print("Playing shift right")
+        elif audio_params[0] == "Stop":
+            print("Play stop")
+        elif audio_params[0] == "Person" and audio_params[2] == "True":
+            print("Play possible person collision detected")
+        elif audio_params[0] == "Car" and audio_params[2] == "True":
+            print("Play possible car collision detected")
+
+        # Exit once full audio clip has been played
 
 
-def add_new_sound(clip_name):
-    prio = audioClipDictionary.get(clip_name)
-    if len(audioClips) >= 1:
-        for i in range(0, len(audioClips)):
-            if i == len(audioClips) - 1:
-                if prio < audioClipDictionary.get(audioClips[i]):
-                    audioClips.insert(i, clip_name)
-                else:
-                    audioClips.append(clip_name)
-                break
-            else:
-                print(str(prio) + " " + clip_name + " " + str(audioClipDictionary.get(audioClips[i])) + " " + audioClips[i])
-                if prio < audioClipDictionary.get(audioClips[i]):
-                    audioClips.insert(i, clip_name)
-                    break
-    else:
-        audioClips.append(clip_name)
 
 
-def play_first_queue_sound():
-    global wave_object
-    wave_object = sa.WaveObject.from_wave_file(audioClips[0])
-    global play_object
-    play_object = wave_object.play()
-    print(play_object.is_playing())
-    print(audioClips)
-    audioClips.remove(audioClips[0])
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# audioClipDictionary = {
+#     "personahead.wav": 1,
+#     "personleft.wav": 2,
+#     "personright.wav": 3,
+#     "stopsignmiddle.wav": 4,
+#     "stopsignleft.wav": 5,
+#     "stopsignright.wav": 6,
+# }
+#
+# audioClips = []
+# wave_object = None
+# play_object = None
+# is_playing = False
+#
+#
+# def add_new_sound(clip_name):
+#     prio = audioClipDictionary.get(clip_name)
+#     if len(audioClips) >= 1:
+#         for i in range(0, len(audioClips)):
+#             if i == len(audioClips) - 1:
+#                 if prio < audioClipDictionary.get(audioClips[i]):
+#                     audioClips.insert(i, clip_name)
+#                 else:
+#                     audioClips.append(clip_name)
+#                 break
+#             else:
+#                 print(str(prio) + " " + clip_name + " " + str(audioClipDictionary.get(audioClips[i])) + " " + audioClips[i])
+#                 if prio < audioClipDictionary.get(audioClips[i]):
+#                     audioClips.insert(i, clip_name)
+#                     break
+#     else:
+#         audioClips.append(clip_name)
+#
+#
+# def play_first_queue_sound():
+#     global wave_object
+#     wave_object = sa.WaveObject.from_wave_file(audioClips[0])
+#     global play_object
+#     play_object = wave_object.play()
+#     print(play_object.is_playing())
+#     print(audioClips)
+#     audioClips.remove(audioClips[0])
+#
