46c14500ab16057b6c5724edf8e746524ca888c5
Alexis Kim <ajkim0630@gmail.com>
2021-06-06 01:06:47 -0700

    started navigation

---
+++ navigation/navigation.py
@@ -0,0 +1,12 @@
+api_key = '5b3ce3597851110001cf624893d90c21680948129afaa561366cd798'
+import openrouteservice as ors
+import json
+
+client = ors.Client(key=api_key)
+
+coords = [[-122.1305079,37.4349431],[-122.1576698,37.4358156]]
+route = client.directions(coordinates=coords,profile='driving-car',format='geojson')
+
+j = open("route.json","w")
+json.dump(route, j, indent = 1)
+j.close()
