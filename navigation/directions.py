0e91d415c9de98a2e61a36ec27599e02246ae135
Alexis Kim <ajkim0630@gmail.com>
2021-06-24 17:24:23 -0700

    pelias search command made simpler

--- navigation/directions.py
+++ navigation/directions.py
@@ -6,7 +6,6 @@ import constants
 client = ors.Client(key=constants.api_key)
 
 def get_directions(starting_coords, ending_coords):
-    # coords = [[-122.1305079,37.4349431],[-122.1576698,37.4358156]]
     coords = [starting_coords, ending_coords]
 
     route = client.directions(coordinates=coords,profile='driving-car',format='geojson')
--- navigation/navigation.py
+++ navigation/navigation.py
@@ -1,35 +1,46 @@
 import constants
 import openrouteservice as ors
-import speech_recognition as sr
 import json
-import gps_state
-
+# import gps_state
+import speech_2_text
 
 client = ors.Client(key=constants.api_key)
 
+coords = [[], []]
+
+
 # when given an address or place as a string, finds coordinates and other info
-def geocode(name):
-    route = client.pelias_search(name, size=1)
+def geocode(name, start):  # start = True means you are geocoding for the start location
+    if start == False:
+        circle_center = coords[0]
+        route = client.pelias_search(name, size=1, circle_point=circle_center, circle_radius=5)
+    else:
+        route = client.pelias_search(name, size=1)
+
+    if (route["features"] == []):
+        return None
     return route
 
 
-start = geocode(gps_state.get_current_address())
-destination = geocode("Starbucks, Mountain View, CA 94043") # use speech to text
-print(start, destination)
-# j = open("start.json", "w")
-# json.dump(start, j, indent=1)
-# j.close()
-#
-# j = open("destination.json", "w")
-# json.dump(destination, j, indent=1)
-# j.close()
-
-coords = [[], []]
+start = geocode("50 Embarcadero Road, Palo Alto, CA 94301", True)
 coords[0] = start["features"][0]["geometry"]["coordinates"]
-coords[1] = destination["features"][0]["geometry"]["coordinates"]
 
-route = client.directions(coordinates=coords, profile='driving-car', format='geojson', language='en')
+# start = geocode(gps_state.get_current_address())
 
-j = open("route.json", "w")
-json.dump(route, j, indent=1)
-j.close()
+destination_speech = speech_2_text.get_speech_to_text()
+destination = geocode(destination_speech, False)
+# print(start, destination)
+
+
+
+
+if destination == None:
+    print("No such location found. Try again.")
+
+else:
+    print ("Getting directions...")
+    coords[1] = destination["features"][0]["geometry"]["coordinates"]
+    route = client.directions(coordinates=coords, profile='foot-walking', format='geojson', language='en')
+    j = open("route.json", "w")
+    json.dump(route, j, indent=1)
+    j.close()
